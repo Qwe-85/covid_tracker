9208b56206e46f199ef010780ff25c753a06295b
Anishglory <anishshrestha@tuicms.edu.np>
2023-12-20 17:33:48 +0545

    new changes

---
+++ lib/Services/states-services.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+
+import 'package:covid_tracker/Model/WorldStatesModel.dart';
+import 'package:covid_tracker/Services/Utilities/app_url.dart';
+import 'package:http/http.dart' as http;
+
+class StatesSercices {
+  Future<WorldStatesModel> fetchWorldStatesRecords() async {
+    final response = await http.get(Uri.parse(AppUrl.worldStatesApi));
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      return WorldStatesModel.fromJson(data);
+    } else {
+      throw Exception('Error');
+    }
+  }
+}
